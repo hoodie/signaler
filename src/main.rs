f8d59ed3f296ad5ebc7daf1382aff9624b1984f8
Hendrik Sollich <hendrik@hoodie.de>
2019-01-12 21:35:33 +0100

    basic ClientSession and Server Messaging

--- src/main.rs
+++ src/main.rs
@@ -1,85 +1,30 @@
+#![allow(unused_imports)]
 // extern crate actix;
-use log::{debug, info, trace, warn};
 use env_logger::{self, Env};
+use log::{debug, info, trace, warn};
 
-use ::actix::prelude::*;
 use actix_web::server::HttpServer;
 use actix_web::{fs, ws, App, Error, HttpRequest, HttpResponse};
 
 use std::{env, error};
 
-// mod server;
-mod protocol;
+pub mod protocol;
+pub mod session;
+pub mod server;
 
-use crate::protocol::*;
+use crate::session::*;
 
 const LOG_VAR: &str = "SIGNALIZER_LOG";
 const BIND_TO: &str = "127.0.0.1:8080";
 
-#[derive(Default)]
-struct SignalingSession {
-    // server_addr: Addr<self::server::SignalingServer>
-}
-
-impl SignalingSession {
-    fn handle_message(&self, raw_msg: &str, ctx: &mut actix_web::ws::WebsocketContext<SignalingSession>) {
-
-        trace!("handle message: {:?}", raw_msg);
-        let parsed: Result<SignalMessage, _> = serde_json::from_str(raw_msg);
-        if let Ok(msg) = parsed {
-            debug!("parsed: {:#?}", msg);
-            match msg {
-                SignalMessage{kind: SignalKind::ShutDown } => {
-                    debug!("received shut down signal");
-                    System::current().stop();
-                }
-                _ => {}
-            }
-            ctx.text(serde_json::to_string(&SignalMessage::ok()).unwrap());
-
-        } else {
-            let default_message = serde_json::to_string(&SignalMessage::err(format!("CantParse!\n{}", Self::allowed_messages()))).unwrap();
-            warn!("cannot parse: {:#?}", raw_msg);
-            ctx.text(default_message);
-        }
-    }
-
-    fn allowed_messages() -> String {
-        format!("{}\n{}",
-                serde_json::to_string_pretty(&SignalMessage::join()).unwrap(),
-                serde_json::to_string_pretty(&SignalMessage::list()).unwrap(),
-                )
-    }
-
-}
-
-impl Actor for SignalingSession {
-    type Context = ws::WebsocketContext<Self>;
-    fn started(&mut self, ctx: &mut Self::Context) {
-        debug!("session started")
-    }
-}
-
-impl StreamHandler<ws::Message, ws::ProtocolError> for SignalingSession {
-    fn handle(&mut self, msg: ws::Message, ctx: &mut Self::Context) {
-        match msg {
-            ws::Message::Ping(msg) => ctx.pong(&msg),
-            ws::Message::Text(text) => self.handle_message(&text, ctx),
-            _ => (),
-       }
-    }
-
-}
-
 fn ws_route(req: &HttpRequest<()>) -> Result<HttpResponse, Error> {
-    trace!("chat route: {:?}", req);
-    ws::start(req, SignalingSession::default())
+    // debug!("chat route: {:?}", req);
+    ws::start(req, ClientSession::default())
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>>{
-
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     if env::var(LOG_VAR).is_err() {
-        env::set_var(LOG_VAR, "signalizer=debug,actix_web=info");
+        env::set_var(LOG_VAR, "signalizer=trace,actix_web=info");
     }
 
     env_logger::init_from_env(Env::new().filter(LOG_VAR));
@@ -96,8 +41,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>>{
                     .index_file("index.html"),
             )
     })
-        .bind(BIND_TO)?
-        .start();
+    .bind(BIND_TO)?
+    .start();
 
     info!("listening on http://{}", BIND_TO);
     sys.run();
--- src/protocol.rs
+++ src/protocol.rs
@@ -1,30 +1,70 @@
-use serde_derive::{Serialize, Deserialize};
 
-/// Message Format
-#[derive(Debug, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct SignalMessage {
-    pub kind: SignalKind,
-}
+pub mod public {
+    use serde_derive::{Deserialize, Serialize};
 
-impl SignalMessage {
-    pub fn ok() -> Self { SignalMessage { kind: SignalKind::Ok} }
-    pub fn join() -> Self { SignalMessage { kind: SignalKind::Join } }
-    pub fn list() -> Self { SignalMessage { kind: SignalKind::List } }
-    pub fn err(msg: impl Into<String>) -> Self {
-        SignalMessage {
-            kind: SignalKind::Err(msg.into())
+    /// Message Format
+    #[derive(Debug, Serialize, Deserialize)]
+    #[serde(rename_all = "camelCase")]
+    pub struct SignalMessage {
+        pub kind: SignalKind,
+    }
+
+    impl SignalMessage {
+        pub fn ok() -> Self {
+            Self::from(SignalKind::Ok)
         }
+
+        pub fn join() -> Self {
+            Self::from(SignalKind::Join{
+                channel: String::from("default"),
+            })
+        }
+
+        pub fn list() -> Self {
+            SignalKind::ListRooms.into()
+        }
+
+        pub fn err(msg: impl Into<String>) -> Self {
+            SignalKind::Err(msg.into()).into()
+        }
+
+        /// dev convenience only!
+        pub fn any<T: serde::Serialize>(anything: T) -> Self {
+            Self::from(SignalKind::Any(serde_json::to_value(&anything).unwrap()))
+        }
+
+        pub fn to_json(self) -> String {
+            serde_json::to_string(&self).unwrap()
+        }
+    }
+
+    impl From<SignalKind> for SignalMessage {
+        fn from(kind: SignalKind) -> Self {
+            SignalMessage { kind }
+        }
+    }
+
+    /// Message Format
+    #[derive(Debug, Serialize, Deserialize)]
+    #[serde(rename_all = "camelCase")]
+    pub enum SignalKind {
+        Join { channel: String },
+        Any ( serde_json::Value ),
+        ListRooms,
+        ShutDown,
+        Ok, // 200
+        Err(String),
     }
 }
 
-/// Message Format
-#[derive(Debug, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub enum SignalKind {
-    Join,
-    List,
-    ShutDown,
-    Ok, // 200
-    Err(String)
+pub mod internal {
+    use actix::prelude::*;
+
+    // #[derive(Message)]
+    // #[rtype(result = "Vec<String>")]
+    pub struct ListRooms;
+
+    impl actix::Message for ListRooms {
+        type Result = Vec<String>;
+    }
 }
