06bbf5a95d53d3662283e6f980ee5bedbb57df34
Hendrik Sollich <hendrik@hoodie.de>
2019-01-27 16:06:56 +0100

    add minimal session class

--- client-lib/src/index.ts
+++ client-lib/src/index.ts
@@ -1,5 +1,61 @@
-export { Command, Message, handleMessage } from './protocol'
+import { Command, Message, ChatMessage } from './protocol'
+
 export function hiThere() {
     console.info("hi there")
 }
 
+export class Session {
+    private connection: WebSocket;
+
+    constructor(url: string) {
+        this.connection = new WebSocket(url);
+        this.connection.onmessage = (rawMsg: MessageEvent) => {
+            try {
+                this.handle(JSON.parse(rawMsg.data));
+            } catch (e) {
+                console.error("can't parse", rawMsg.data);
+            }
+        };
+    }
+
+    private handle(msg: Message) {
+        switch (msg.type) {
+            case 'welcome': return console.info("welcome", msg.session);
+            case 'roomList': return this.handleRooms(msg.rooms);
+            case 'myRoomList': return this.handleMyRooms(msg.rooms);
+            case 'message': return this.handleChatMessage(msg.message);
+            case 'any': return console.debug(msg.payload);
+            default: return console.warn('unhandle message', msg);
+        }
+    }
+
+    public handleRooms = (rooms: string[]) =>
+        console.info('all possible rooms' , rooms );
+
+    public handleMyRooms = (rooms: string[]) =>
+        console.info("I'm participating in " , rooms );
+
+    public handleChatMessage = (message: ChatMessage) =>
+        console.info('❤️ received', { message })
+
+    public sendCommand(cmd: Command) {
+        this.connection.send(JSON.stringify(cmd));
+    }
+
+    public join(room: string) {
+        this.sendCommand({ type: "join", room })
+    }
+
+    public listRooms() {
+        this.sendCommand({ type: "listRooms" })
+    }
+
+    public listMyRooms() {
+        this.sendCommand({ type: "listMyRooms" })
+    }
+
+    public sendMessage(content: string, room: string) {
+        this.sendCommand({ type: "message", message: { content }, room })
+    }
+
+}
--- client-lib/src/protocol.ts
+++ client-lib/src/protocol.ts
@@ -1,15 +1,34 @@
+export interface CommandJoin { type: "join", room: string }
+export interface CommandMessage { type: "message", message: { content: string }, room: string }
+export interface CommandListRooms { type: "listRooms" }
+export interface CommandListMyRooms { type: "listMyRooms" }
+export interface CommandShutdown { type: "shutDown" };
+
 export type Command =
-    | { type: "join", room: string }
-    | { type: "message", message: { content: string }, room: string }
-    | { type: "listRooms" }
-    | { type: "listMyRooms" }
-    | { type: "shutDown" };
+    | CommandJoin
+    | CommandMessage
+    | CommandListRooms
+    | CommandListMyRooms
+    | CommandShutdown;
 
+export interface MessageWelcome { type: 'welcome', session: { uuid: string } }
+export interface MessageRoomList { type: 'roomList', rooms: string[] }
+export interface MessageMyRoomList { type: 'myRoomList', rooms: string[] }
+export interface MessageMessage { type: 'message', message: ChatMessage }
+export interface MessageAny { type: 'any', payload: any }
+export interface MessageOk { type: 'ok' }
 
-export type Message = {
-    type: 'welcome'
-    session: { uuid: string }
+export interface ChatMessage {
+    content: string;
 }
+
+export type Message =
+    | MessageWelcome
+    | MessageRoomList
+    | MessageMyRoomList
+    | MessageMessage
+    | MessageAny
+    | MessageOk;
 
 export function handleMessage(msg: Message) {
     switch (msg.type)  {
