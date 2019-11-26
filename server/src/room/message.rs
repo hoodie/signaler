7fa335e4ceaa5956b20398989c73850322b8a038
Hendrik Sollich <hendrik@hoodie.de>
2019-11-26 21:56:23 +0100

    break up room module

---
+++ server/src/room/message.rs
@@ -0,0 +1,33 @@
+use actix::prelude::*;
+use actix::WeakAddr;
+use signaler_protocol::{ChatMessage, Participant};
+
+use super::{DefaultRoom, RoomId};
+
+#[derive(Message, Debug)]
+#[rtype(result = "()")]
+pub enum RoomToSession {
+    Joined(RoomId, WeakAddr<DefaultRoom>),
+    ChatMessage {
+        room: RoomId,
+        message: ChatMessage,
+    },
+
+    History {
+        room: RoomId,
+        messages: Vec<ChatMessage>,
+    },
+
+    RoomState {
+        room: RoomId,
+        participants: Vec<Participant>,
+    },
+
+    JoinDeclined {
+        room: RoomId,
+    },
+
+    Left {
+        room: RoomId,
+    },
+}
