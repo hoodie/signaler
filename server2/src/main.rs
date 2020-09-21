1a96de5adcb26ea6801f1729d0151bd291a8cb38
Hendrik Sollich <hendrik@hoodie.de>
2020-09-21 19:38:02 +0200

    refactor: build it again with xactor, smol and warp

---
+++ server2/src/main.rs
@@ -0,0 +1,57 @@
+#![allow(unused_imports)]
+use async_compat::{Compat, CompatExt};
+use env_logger::Env;
+use warp::{http::Uri, ws::WebSocket, Filter};
+
+use std::env;
+
+mod connection;
+
+const LOG_VAR: &str = "LOG";
+async fn peer_connected(ws: WebSocket /*, broker: Broker*/) {
+    log::debug!("user connected{:#?}", ws);
+
+    let connection = crate::connection::Connection::new(ws);
+    let addr = xactor::Actor::start(connection).await.unwrap();
+    addr.send(connection::commands::SayWelcome).unwrap();
+    addr.wait_for_stop().await
+
+    // let mut peer = Peer::new(ws, broker.addr());
+    // peer.register_at_broker();
+    // peer.send_welcome().await;
+    // peer.start().await;
+}
+
+fn main() {
+    color_backtrace::install();
+
+    if env::var(LOG_VAR).is_err() {
+        env::set_var(LOG_VAR, "server2=trace,warp=info");
+    }
+
+    env_logger::init_from_env(Env::new().filter(LOG_VAR));
+
+    let routes = {
+        let channel = warp::path("ws")
+            .and(warp::ws())
+            //.and(broker)
+            .map(|ws: warp::ws::Ws /*, broker: Broker*/| {
+                #[allow(clippy::redundant_closure)]
+                ws.on_upgrade(move |socket| peer_connected(socket /*, broker*/))
+            });
+        let app = warp::path("app").and(warp::fs::dir("../static/"));
+        let redirect_to_app = warp::any().map(|| warp::redirect(Uri::from_static("/app/")));
+
+        let hello = warp::path("hello").map(|| {
+            log::info!("✉️ hello world");
+            "Hello, World!"
+        });
+        app.or(hello).or(channel).or(redirect_to_app)
+    };
+
+    let main_socket = std::net::SocketAddr::from(([0, 0, 0, 0], 3030));
+
+    smol::block_on(Compat::new(async {
+        warp::serve(routes).run(main_socket).await;
+    }));
+}
