e0dfad4e49298c30e413bdd5a23a55107c9e8b9a
Hendrik Sollich <hendrik@hoodie.de>
2020-10-09 12:58:33 +0200

    WIP

--- server2/src/main.rs
+++ server2/src/main.rs
@@ -1,21 +1,19 @@
 #![allow(unused_imports)]
 use async_compat::{Compat, CompatExt};
+use async_std::future::Future;
 use env_logger::Env;
-use warp::{http::Uri, ws::WebSocket, Filter};
 
+use async_std::prelude::*;
 use std::env;
 
 mod connection;
+mod session;
+mod web_server;
 
 const LOG_VAR: &str = "LOG";
-async fn peer_connected(ws: WebSocket /*, broker: Broker*/) {
-    log::debug!("user connected{:#?}", ws);
-    let connection = connection::Connection::new(ws);
-    let addr = xactor::Actor::start(connection).await.unwrap();
-    addr.wait_for_stop().await
-}
 
-fn main() {
+#[async_std::main]
+async fn main() {
     color_backtrace::install();
 
     if env::var(LOG_VAR).is_err() {
@@ -24,27 +22,20 @@ fn main() {
 
     env_logger::init_from_env(Env::new().filter(LOG_VAR));
 
-    let routes = {
-        let channel = warp::path("ws")
-            .and(warp::ws())
-            //.and(broker)
-            .map(|ws: warp::ws::Ws /*, broker: Broker*/| {
-                #[allow(clippy::redundant_closure)]
-                ws.on_upgrade(move |socket| peer_connected(socket /*, broker*/))
-            });
-        let app = warp::path("app").and(warp::fs::dir("../static/"));
-        let redirect_to_app = warp::any().map(|| warp::redirect(Uri::from_static("/app/")));
+    let server = async_std::task::spawn(async {
+        let main_socket = std::net::SocketAddr::from(([0, 0, 0, 0], 3030));
+        xactor::Supervisor::start(move || web_server::WebServer::new(main_socket))
+            // xactor::Supervisor::start(session::Session::default)
+            .await
+            .unwrap()
+    });
+    let session = async_std::task::spawn(async {
+        xactor::Supervisor::start(session::Session::default).await.unwrap()
+    });
 
-        let hello = warp::path("hello").map(|| {
-            log::info!("✉️ hello world");
-            "Hello, World!"
-        });
-        app.or(hello).or(channel).or(redirect_to_app)
-    };
+    let session2 = async_std::task::spawn(async {
+        xactor::Supervisor::start(session::Session::default).await.unwrap()
+    });
 
-    let main_socket = std::net::SocketAddr::from(([0, 0, 0, 0], 3030));
-
-    smol::block_on(Compat::new(async {
-        warp::serve(routes).run(main_socket).await;
-    }));
+    futures::join!(server, session, session2);
 }
---
+++ server2/src/session.rs
@@ -0,0 +1,32 @@
+use uuid::Uuid;
+use xactor::{Actor, Context, Handler};
+
+pub struct Session {
+    session_id: Uuid,
+}
+
+impl Default for Session {
+    fn default() -> Self {
+        Session {
+            session_id: Uuid::new_v4(),
+        }
+    }
+}
+
+#[async_trait::async_trait]
+impl Actor for Session {
+    async fn started(&mut self, ctx: &mut xactor::Context<Self>) -> xactor::Result<()> {
+        log::info!("starting session on actor {:?}", ctx.actor_id());
+        Ok(())
+    }
+}
+
+#[xactor::message]
+struct Foo;
+
+#[async_trait::async_trait]
+impl Handler<Foo> for Session {
+    async fn handle(&mut self, _ctx: &mut xactor::Context<Self>, _msg: Foo) {
+        log::info!("received a Foo");
+    }
+}
